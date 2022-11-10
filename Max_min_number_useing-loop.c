261c6509d0d7769e95bf899bd78f3e949a38f913
Marut Lashkari <98412051+marutlashkari@users.noreply.github.com>
2022-11-10 17:36:45 +0530

    Update and rename maxmi.c to Max_min_number_useing-loop.c

--- Max_min_number_useing-loop.c
+++ Max_min_number_useing-loop.c
@@ -3,22 +3,19 @@ void main()
 {
 	int num[10],i,min=0,max=0,avg=0;
 
-	for(i=0;i<10;i++)
-	{
+	for(i=0;i<10;i++){
+		
 		printf("enter array element:");
 		scanf("%d",&num[i]);
 	}
-	for(i=0;i<10;i++)
-	{
-		if(num[0]>=num[i])
-		{
+	
+	for(i=0;i<10;i++){
+		if(num[0]>=num[i]){	
  			printf("%d",max);
 		}
-		else
-		{
+		else{
 			printf("%d",min);
 		}
-
 	}
 	printf("max=%d,min=%d",max,min);
 
