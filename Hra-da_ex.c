19c564e47f0dca8b99af588a0a60e919ee2cbade
Marut Lashkari <98412051+marutlashkari@users.noreply.github.com>
2022-11-10 17:52:50 +0530

    Update and rename sa.c to Hra-da_ex.c

--- Hra-da_ex.c
+++ Hra-da_ex.c
@@ -4,25 +4,32 @@ void main()
 	int bs,gs,hra,da;
 	printf("enter bs");
 	scanf("%d",&bs);
-	if(bs>=10000 && bs<20000)
-	{
+	
+	if(bs>=10000 && bs<20000){
+		
 		hra=(20*bs)/100;
 		da=(80*bs)/100;
+		
 	}
-	else if(bs>=20000 && bs<30000)
-	{
+	
+	else if(bs>=20000 && bs<30000){
+		
 		hra=(25*bs)/100;
 		da=(90*bs)/100;
 	}
-	else if(bs>=30000)
-	{
+	
+	else if(bs>=30000){
+		
 		hra=(30*bs)/100;
 		da=(95*bs)/100;
 	}
-	else 
-	{
-	printf("input sallry lase than 10000");
+	
+	else {
+		
+		printf("input sallry lase than 10000");
+		
 	}
+	
 	gs=bs+da+hra;
 	printf("gs=%d",gs);
 	
