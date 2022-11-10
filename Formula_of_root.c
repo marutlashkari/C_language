1731eb8ca93a28fffa7f061f3bf25fba207119b4
Marut Lashkari <98412051+marutlashkari@users.noreply.github.com>
2022-11-10 17:48:55 +0530

    Update and rename root.c to Formula_of_root.c

--- Formula_of_root.c
+++ Formula_of_root.c
@@ -12,23 +12,26 @@ int main()
 
 	d=((b*b)-(4*a*c));
 
-	if(d>0)
-	{
+	if(d>0){
+		
 		r1=((-b)+sqrt(d)/(2*a));
 		r2=((-b)+sqrt(d)/(2*a));
 		printf("r1=%d"r1);
 		printf("r2=%d"r2);
+		
 	}
-	else if (d==0)
-	{
+	else if (d==0){
+		
 		f=(-b)/(2*a);
 		printf("f=%d"f);
+		
 	}
-	else if (d<0)
-	{
+	else if (d<0){
+		
 		p=((-b)/2*a)+i sqrt((d)/(2*a));
 		q=((-b)/2*a)-i sqrt((d)/(2*a));
 		printf("p=%d"p);
 		printf("q=%d"q);
+		
 	}
 }
