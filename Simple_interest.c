0aea29e768f5563e47cdb32a627ead341ac0b58a
Marut Lashkari <98412051+marutlashkari@users.noreply.github.com>
2022-11-11 15:13:21 +0530

    Update Simple_interest.c

--- Simple_interest.c
+++ Simple_interest.c
@@ -5,10 +5,14 @@ void main ()
 	
 	printf("enter p=");
 	scanf("%f",&p);
+	
 	printf("enter n=");
 	scanf("%f",&n);
+	
 	printf("enter r=");
 	scanf("%f",&r);
+	
 	simple=(p*n*r)/100;
+	
 	printf("simpul int=%f",simple);
 }
