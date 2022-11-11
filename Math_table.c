c276d27b489ef8649f5bd66142ed9cf7439c4e61
Marut Lashkari <98412051+marutlashkari@users.noreply.github.com>
2022-11-11 15:24:52 +0530

    Update and rename table.c to Math_table.c

---
+++ Math_table.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int i, no;
+
+	scanf("%d",&no);
+	
+	for(i=1; i<10; i++){
+		
+		printf(no +" X "+ i +" = "+i*no);
+		
+	}
+	
+}
--- table.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	int i,j,sum,no1,no2;
-
-	scanf("%d",&no1);
-	scanf("%d",&no2);
-}
